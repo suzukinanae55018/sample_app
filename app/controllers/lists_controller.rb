881fe7298b89f22982483f14a7a58b68bb89c434
EC2 Default User <ec2-user@ip-172-31-36-235.ap-northeast-1.compute.internal>
2024-05-04 12:21:47 +0000

    [Finish] 3-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,14 +6,16 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
   def index
     @lists = List.all
   end
 
+# 1件だけ取得するのでインスタンス変数名は単数形
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
