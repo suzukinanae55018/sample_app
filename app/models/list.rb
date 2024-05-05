b30692ca566cd978d5f0be2e9924e0d465be343e
EC2 Default User <ec2-user@ip-172-31-36-235.ap-northeast-1.compute.internal>
2024-05-05 12:24:01 +0000

    [Finish] 3-8

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,8 @@
 class List < ApplicationRecord
   has_one_attached  :image
+  
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true
 end
 # ActiveStorageをインストール後、ここにhas_one_attachedメソッドを追加(画像をアップするのに必要)
